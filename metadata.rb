a55da38f4072e9a000c6b088db8eb17ee3b4e77a
Jamie Starke <github@jamiestarke.com>
2015-03-07 22:09:26 -0800

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'zerotier'
+maintainer       'Jamie Starke'
+maintainer_email 'github@jamiestarke.com'
+license          'Apache 2.0'
+description      'Installs/Configures zerotier'
+version          '0.1.0'
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
