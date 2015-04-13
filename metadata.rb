bab19cad9fa596cb3aa5e1ccc238000902e6836f
Jamie Starke <jrstarke@gmail.com>
2015-04-12 22:11:24 -0700

    Incremented version number to allow for retries

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Jamie Starke'
 maintainer_email 'github@jamiestarke.com'
 license          'Apache 2.0'
 description      'Installs/Configures zerotier'
-version          '0.1.0'
+version          '0.1.1'
 
 %w{ ubuntu }.each do |os|
   supports os
