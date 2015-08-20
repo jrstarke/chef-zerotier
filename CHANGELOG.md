e21dc9e561647576e8eb143945316b5cada46afc
Jamie Starke <github@jamiestarke.com>
2015-08-19 17:01:14 -0700

    Ipdated the version number and change logs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.2
+
+Update of the installer_url to version 1.0.4
+
 # 0.1.0
 
 Initial release of zerotier cookbook
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Jamie Starke'
 maintainer_email 'github@jamiestarke.com'
 license          'Apache 2.0'
 description      'Installs/Configures zerotier'
-version          '0.1.1'
+version          '0.1.2'
 
 %w{ ubuntu }.each do |os|
   supports os
